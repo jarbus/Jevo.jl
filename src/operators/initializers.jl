512ce761e87555b698e5e48496be5afdcaa46d5f
Jack <jackgarbus@gmail.com>
2024-02-27 11:54:40 -0500

    fix: pops no longer regenerated every gen

--- src/operators/initializers.jl
+++ src/operators/initializers.jl
@@ -2,6 +2,7 @@ export InitializeAllPopulations
 """Samples 1 population from each population creator and puts them into state.populations"""
 @define_op "InitializeAllPopulations"
 InitializeAllPopulations(;kwargs...) = create_op("InitializeAllPopulations",
+          condition=first_gen,
           retriever=PopulationCreatorRetriever(),
           operator=create,
           updater=PopulationAdder(),kwargs...)
