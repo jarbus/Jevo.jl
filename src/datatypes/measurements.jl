a879952df8a282655077eb09617add18096c46a9
Jack <jackgarbus@gmail.com>
2024-03-09 12:13:53 -0500

    log  chg  3-digit precision for measurements

--- src/datatypes/measurements.jl
+++ src/datatypes/measurements.jl
@@ -20,6 +20,6 @@ end
 
 measure(metric::AbstractMetric, ::AbstractState) = @error "No measure function defined for $(typeof(metric))"
 
-Base.show(io::IO, m::Measurement) = print(io, "gen=$(m.generation) $(m.metric)=$(round(m.value, digits=2))")
-Base.show(io::IO, m::StatisticalMeasurement) = print(io, 
-    "gen=$(m.generation) $(m.metric): |$(round(m.min, digits=2)), $(round(m.mean, digits=2)) ± $(round(m.std, digits=2)), $(round(m.max, digits=2))|, $(m.n_samples) samples")
+Base.show(io::IO, m::Measurement; digits::Int=3) = print(io, "gen=$(m.generation) $(m.metric)=$(round(m.value, digits=digits))")
+Base.show(io::IO, m::StatisticalMeasurement; digits::Int=3) = print(io, 
+    "gen=$(m.generation) $(m.metric): |$(round(m.min, digits=digits)), $(round(m.mean, digits=digits)) ± $(round(m.std, digits=digits)), $(round(m.max, digits=digits))|, $(m.n_samples) samples")
