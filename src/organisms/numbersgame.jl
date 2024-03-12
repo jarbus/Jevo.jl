3e307b9247b85ee21c5ea5bb392bf95004ed04a8
jarbus <jarbus@tutanota.com>
2024-03-12 11:18:10 -0400

    ng   fix  make mutate compatible w/ multithread rng

--- src/organisms/numbersgame.jl
+++ src/organisms/numbersgame.jl
@@ -10,15 +10,15 @@ end
 
 VectorGenotype(n::Int, rng::AbstractRNG; init::Function=rand) = VectorGenotype(init(rng, Float32, n))
 
-function mutate(state::State, genotype::VectorGenotype)
+function mutate(rng::AbstractRNG, state::State, genotype::VectorGenotype)
     # add random noise to two random dimensions
-    i = rand(state.rng, 1:length(genotype.numbers))
-    j = rand(state.rng, 1:length(genotype.numbers))
+    i = rand(rng, 1:length(genotype.numbers))
+    j = rand(rng, 1:length(genotype.numbers))
     while i == j
-        j = rand(state.rng, 1:length(genotype.numbers))
+        j = rand(rng, 1:length(genotype.numbers))
     end
-    genotype.numbers[i] += randn(state.rng)
-    genotype.numbers[j] += randn(state.rng)
+    genotype.numbers[i] += randn(rng)
+    genotype.numbers[j] += randn(rng)
     genotype
 end
 
