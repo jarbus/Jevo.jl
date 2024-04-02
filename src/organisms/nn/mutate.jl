078a73cb0033ffe1c1bc6b5394ed4b59f4ca8317
Jack <jackgarbus@gmail.com>
2024-04-02 13:47:37 -0400

    nn   fix  new mr for each gene

--- src/organisms/nn/mutate.jl
+++ src/organisms/nn/mutate.jl
@@ -8,15 +8,16 @@ function compute_init(fn::Function)
     fn
 end
 
-function mutate_weights!(rng::AbstractRNG, state::State, genotype::Network, mr::Float32; n=-1)
+function mutate_weights!(rng::AbstractRNG, state::State, genotype::Network, mr::Union{Float32,Tuple{Vararg{Float32}}}; n=-1)
     gene_counter = get_counter(AbstractGene, state)
     weights = get_weights(rng, genotype, n=n)
     for weight in weights
+        mrf0 = mr isa Float32 ? mr : rand(rng, mr)
         init = compute_init(weight.muts[1].init!)
         if rand(rng) > 0.05 # Generate new gene with 95% probability
-            push!(weight.muts, NetworkGene(rng, gene_counter, mr, init))
+            push!(weight.muts, NetworkGene(rng, gene_counter, mrf0, init))
         else # Re-apply existing gene with 5% probability
-            push!(weight.muts, NetworkGene(gene_counter, weight.muts[end].seed, mr, init))
+            push!(weight.muts, NetworkGene(gene_counter, weight.muts[end].seed, mrf0, init))
         end
     end
 end
@@ -25,8 +26,7 @@ function mutate(rng::AbstractRNG, state::State, genotype::Network; mr::Union{Flo
     @assert genotype.coupling in (StrictCoupling, NoCoupling) "Only strict and no coupling are supported right now"
     new_genotype = deepcopy(genotype)
     genotype.coupling == StrictCoupling && (n = -1)
-    mrf0 = mr isa Float32 ? mr : rand(rng, mr)
-    mutate_weights!(rng, state, new_genotype, mrf0, n=n)
+    mutate_weights!(rng, state, new_genotype, mr, n=n)
     new_genotype
 end
 
