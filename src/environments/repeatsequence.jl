c85ab07658b9a0e91f1955f8a22a2ba2ea27941e
jarbus <jarbus@tutanota.com>
2024-09-01 18:29:14 -0400

    nn   fix  OOM

--- src/environments/repeatsequence.jl
+++ src/environments/repeatsequence.jl
@@ -88,8 +88,7 @@ function get_preprocessed_batch(env::RepeatSequence, tfr)
     # Allocating a large amount of memory on the CPU appears to alleviate this 
     # issue. Garbage collection does not help. Unable to justify spending
     # more time on this, if it's resolved. On my laptop, this takes ~179μs per call
-    #size(zeros(1_000_000)) # TODO see if we can change this to fill(undef, 1_000_000)
-    size(fill(undef, 1_000_000))
+    size(zeros(1_000_000))
     Main.preprocessed_batch |> deepcopy |> gpu
 end
 
