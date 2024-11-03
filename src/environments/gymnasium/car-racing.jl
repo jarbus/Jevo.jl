8ea73929586a49a02b5a1c3f2bcd706c6556b994
Jack <jackgarbus@gmail.com>
2024-11-03 16:20:08 -0500

    wrapper reorder

--- src/environments/gymnasium/car-racing.jl
+++ src/environments/gymnasium/car-racing.jl
@@ -16,9 +16,6 @@ function CarRacingV3(n_envs::Int, n_steps::Int, skip::Int)
     return CarRacingV3(n_envs, n_steps, skip, nothing, nothing, false)
 end
 
-using Random
-using Distributions
-
 #= function sample_once_per_column(rng, matrix::Matrix) =#
 #=     n_cols = size(matrix, 2) =#
 #=     samples = Vector{Int}(undef, n_cols) =#
@@ -48,10 +45,10 @@ function step!(env::CarRacingV3, ids::Vector{Int}, phenotypes::Vector)
         def make_single_env(gym, np, n_steps, n_stack):
             def anonymous_env_creator():
                 _env = gym.make("CarRacing-v3")
-                _env = gym.wrappers.TimeLimit(_env, max_episode_steps=n_steps)
-                _env = gym.wrappers.FrameStackObservation(_env, stack_size=n_stack)
                 _env = gym.wrappers.DtypeObservation(_env, np.float32)
+                _env = gym.wrappers.TimeLimit(_env, max_episode_steps=n_steps)
                 _env = gym.wrappers.RescaleObservation(_env, min_obs=0.0, max_obs=1.0)
+                _env = gym.wrappers.FrameStackObservation(_env, stack_size=n_stack)
                 return _env
             return anonymous_env_creator
         """ => make_single_env
@@ -65,21 +62,31 @@ function step!(env::CarRacingV3, ids::Vector{Int}, phenotypes::Vector)
         obs = permutedims(reshape(obs, size(obs, 1), size(obs, 2)*size(obs, 5), size(obs, 3), size(obs, 4)), (3, 4, 2, 1))
         env.prev_obs = obs
     end
+    start_time = time()
     actions = phenotypes[1](env.prev_obs) |> cpu
+    forward_pass_time = time()
     actions = pyimport("numpy").array(actions).T
-    # sample actions from the probabilities, batch is the last dimension
-    #rng = StableRNG(ids[1])
-    #actions = sample_once_per_column(rng, probs) .- 1 # subtract 1 to make it 0-indexed
-    #actions = pyimport("numpy").array(actions, dtype=pybuiltins.int)
-    #println(actions)
-    obs, reward, terminated, truncated, info = env.env.step(actions)
-    obs = pyconvert(Array{Float32}, obs) |> cu
+    numpy_import_time = time()
+
+    reward = 0.0
+    obs = nothing
+    for _ in 1:env.n_stack
+        obs, _reward, terminated, truncated, info = env.env.step(actions)
+        terminated, truncated = PyArray(terminated), PyArray(truncated)
+
+        if any(terminated) != all(terminated) || any(truncated) != all(truncated)
+            error("Some environments terminated while others didn't")
+        end
+        env.done = env.done || any(terminated) || any(truncated)
+        reward += sum(PyArray(_reward))
+    end
+    env_process_time = time()
+    @info("Forward pass time: $(forward_pass_time - start_time)")
+    @info("Numpy import time: $(numpy_import_time - forward_pass_time)")
+    @info("Env process time: $(env_process_time - numpy_import_time)")
+    @assert !isnothing(obs)
+    obs = PyArray(obs) |> cu
     obs = permutedims(reshape(obs, size(obs, 1), size(obs, 2)*size(obs, 5), size(obs, 3), size(obs, 4)), (3, 4, 2, 1))
     env.prev_obs = obs
-    if Bool(terminated.any()) != Bool(terminated.all()) || 
-        Bool(truncated.any()) != Bool(truncated.all())
-        error("Some environments terminated while others didn't")
-    end
-    env.done = Bool(terminated.any()) || Bool(truncated.any())
-    [Interaction(ids[1], [], sum(PyArray(reward)))]
+    [Interaction(ids[1], [], reward)]
 end
