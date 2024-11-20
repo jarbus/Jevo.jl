442a93e206b3132bf6d5af773c3cceb0226a6903
jarbus <jarbus@tutanota.com>
2024-11-20 10:04:10 -0500

    allow custom env creator for a solo matchmaker

--- src/operators/matchmaker/solo.jl
+++ src/operators/matchmaker/solo.jl
@@ -2,19 +2,22 @@ export SoloMatchMaker
 """
 """
 @define_op "SoloMatchMaker" "AbstractMatchMaker"
-SoloMatchMaker(ids::Vector{String}=String[];kwargs...) =
+SoloMatchMaker(ids::Vector{String}=String[]; env_creator=nothing, kwargs...) =
     create_op("SoloMatchMaker",
           condition=always,
           retriever=PopulationRetriever(ids),
-          operator=make_solo_matches,
+          operator=(s,ps)->make_solo_matches(s, ps, env_creator),
           updater=add_matches!;kwargs...)
 
 
-function make_solo_matches(state::AbstractState, pops::Vector{Vector{Population}})
+function make_solo_matches(state::AbstractState, pops::Vector{Vector{Population}}, env_creator)
     match_counter = get_counter(AbstractMatch, state)
-    env_creators = get_creators(AbstractEnvironment, state)
-    @assert length(env_creators) == 1 "There should be exactly one environment creator for the time being, found $(length(env_creators))."
-    env_creator = env_creators[1]
+    if isnothing(env_creator)
+        env_creators = get_creators(AbstractEnvironment, state)
+        @assert length(env_creators) == 1 "There should be exactly one environment creator for the time being, found $(length(env_creators))."
+        env_creator = env_creators[1]
+    end
+    @assert !isnothing(env_creator) "Environment creator must be defined"
     matches = Vector{Match}()
     pop = pops[1][1]
     for subpop in pops, pop in subpop, ind in pop.individuals
