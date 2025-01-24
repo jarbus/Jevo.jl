d3672eab808e0f8649957eddf7d041e95f39550f
Jack <jackgarbus@gmail.com>
2025-01-24 17:31:10 -0500

    add timestamp to warning

--- src/organisms/nn/develop.jl
+++ src/organisms/nn/develop.jl
@@ -10,7 +10,7 @@ function get_earliest_cached_weight(dims::NTuple{N, Int}, genes::Vector{NetworkG
             return (copy(weights), i)
         end
     end
-    @info "No cached weights found for any genes. Returning zero tensor."
+    @info "[" * Dates.format(Dates.now(), "yy-mm-dd HH:MM:SS") * "] No cached weights found for any genes. Returning zero tensor."
     zeros(Float32, dims), 0
 end
 """
