edd6f6380ce1151226722b82139d89a1b86dd3bc
Jack <jackgarbus@gmail.com>
2025-04-24 22:41:19 -0400

    fix

--- src/environments/trade2.jl
+++ src/environments/trade2.jl
@@ -111,7 +111,7 @@ function log_trade_ratio(state, pops, h5)
                 idx1, idx2 = idx_map[id1], idx_map[id2]
                 trade_matrix_updated[idx1, idx2] = true
                 trade_matrix[idx1,idx2] = max(trade_matrix[idx1,idx2], int.min_resource)
-                if (id1, id2) || (id2, id1) in keys(distances)
+                if (id1, id2) in keys(distances) || (id2, id1) in keys(distances)
                     distance_matrix[idx1,idx2] = distances[(id1, id2)]
                 end
             end
